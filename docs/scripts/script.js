c74abff26c8faf36a7d007d314b3ae3687ee58dc
Aleksandr <aleksandrbartov@yandex.ru>
2018-08-06 03:46:19 +0400

    some great changes

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -1,34 +1,107 @@
-const b = document.getElementById('bapt')
-const t = document.getElementById('test')
+window.addEventListener('load', function(){
 
-window.onload = function () {
-    this.setTimeout(toBegin, 200)
-}
+  const a = document.getElementById('varvara'),
+        b = document.getElementById('ivan'),
+        c = document.getElementById('boris'),
+        box1 = document.getElementById('container')
 
-function toBegin() {
-    b.style.transform = 'translateZ(-60px) translateY(5px) translateX(0px)'
-    b.style.filter = 'blur(0px)'
-    t.style.transform = 'translateZ(-130px) translateY(5px) translateX(300px)'
-    t.style.filter = 'blur(3px)'
-}
+  this.setTimeout(function() {
+    a.classList.remove('untilLoad')
+    b.classList.remove('untilLoad')
+    c.classList.remove('untilLoad')
+    a.classList.add('photo')
+    b.classList.add('photo')
+    c.classList.add('photo')
+    b.classList.add('back--right')
+    c.classList.add('back--right')
+  }, 1000)
+  
+  
 
-var last_known_scroll_position = 0;
-var ticking = false;
-function doSomething() {
-    b.style.transform = 'translateZ(-130px) translateY(5px) translateX(-300px)'
-    b.style.filter = 'blur(1px)'
-    t.style.transform = 'translateZ(-60px) translateY(5px) translateX(0px)'
-    t.style.filter = 'blur(0px)'
-}
 
-window.addEventListener('scroll', function(e) {
-  last_known_scroll_position = window.scrollX;
-  if (!ticking) {
-    window.requestAnimationFrame(function() {
-      doSomething();
-      ticking = false;
-    });
-     
-    ticking = true;
+  // прокрутка влево
+  
+  function scrollToTheLeft () {
+    if ( !a.classList.contains('back--left') && !a.classList.contains('back--right') ) {
+              a.classList.add('back--left')
+              c.classList.remove('back--left')
+              c.classList.add('back--right')
+              b.classList.remove('back--right')
+         } else if ( a.classList.contains('back--left') ) {
+              a.classList.remove('back--left')
+              a.classList.add('back--right')
+              c.classList.remove('back--right')
+              b.classList.add('back--left')
+         } else {
+              a.classList.remove('back--right')
+              c.classList.add('back--left')
+              b.classList.remove('back--left')
+              b.classList.add('back--right')
+        }
+      }
+  
+  // прокрутка вправо
+  
+  function scrollToTheRight () {
+    if ( !a.classList.contains('back--left') && !a.classList.contains('back--right') ) {
+              a.classList.add('back--right')
+              c.classList.remove('back--left')
+              b.classList.remove('back--right')
+              b.classList.add('back--left')
+         } else if ( a.classList.contains('back--left') ) {
+              a.classList.remove('back--left')
+              c.classList.remove('back--right')
+              c.classList.add('back--left')
+              b.classList.add('back--right')
+         } else {
+              a.classList.remove('back--right')
+              a.classList.add('back--left')
+              c.classList.add('back--right')
+              b.classList.remove('back--left')
+        }
+      }
+
+  // выбор статьи
+
+  function toChoose () {
+    if ( !a.classList.contains('back--left') && !a.classList.contains('back--right') ) {
+            a.classList.add('chosen')
+      } else if ( !b.classList.contains('back--left') && !b.classList.contains('back--right') ) {
+            b.classList.add('chosen')
+      } else {
+            c.classList.add('chosen')
+      }
   }
-});
+  
+  // код чтения событий косания ( право/ лево )
+  
+  var startx = 0, dist = 0
+  
+  box1.addEventListener('touchstart', function(e){
+      var touchobj = e.changedTouches[0]
+      startx = parseInt(touchobj.clientX)
+      e.preventDefault()
+    }, false)
+  
+  
+  box1.addEventListener('touchmove', function(e){
+      var touchobj = e.changedTouches[0]
+      dist = parseInt(touchobj.clientX) - startx
+      e.preventDefault()
+    }, false)
+
+  box1.addEventListener('touchend', function(e){
+      if ( dist > 10 ) {
+        scrollToTheRight()
+      } else if ( dist < 10 && dist > -10 ) {
+        toChoose()
+      } else {
+        scrollToTheLeft()
+      }
+      e.preventDefault()
+    }, false)
+
+  },
+  
+  
+  false);
